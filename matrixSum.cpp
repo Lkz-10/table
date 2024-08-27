f6fb19aab496dbdcb3a82a7e37ef2bab0f304ddf
Lkz-10 <gkazarin.10@bk.ru>
2024-08-27 19:50:53 +0300

    nichego lishnego

--- matrixSum.cpp
+++ matrixSum.cpp
@@ -1,14 +1,17 @@
+#include <assert.h>
+#include <stdio.h>
+
 #include "matrixSum.h"
-#include "print2.h"
 
 void Matrix_sum (int* matrix1, int* matrix2, const int sizeY, const int sizeX)
 {
-    const int size_res_y = 4, size_res_x = 3;
-    int result[size_res_y][size_res_x] = {};
+    assert(matrix1);
+    assert(matrix2);
+
     for (int y = 0; y < sizeY; ++y) {
         for (int x = 0; x < sizeX; ++x) {
-            result[y][x] = *((int*)matrix1 + y*sizeX + x) + *((int*)matrix2 + y*sizeX + x);
+            printf("%3d ", *((int*)matrix1 + y*sizeX + x) + *((int*)matrix2 + y*sizeX + x));
         }
+        printf("\n");
     }
-    Print2((int*) result, size_res_y, size_res_x);
 }
