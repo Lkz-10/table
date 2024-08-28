f2dc02e3a3f89b4906230762cb281a0167d80c9b
Lkz-10 <gkazarin.10@bk.ru>
2024-08-28 16:24:04 +0300

    Ввод-вывод двумерного массива с разными длинами строк

--- main.cpp
+++ main.cpp
@@ -1,10 +1,64 @@
-#include "matrixSum.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int Input(int** addr, int* line_lens);
+void Output(int** addr, int* line_lens);
+
+void Free(int** addr);
+
+const int global_nlines = 4;
 
 int main()
 {
-    const int sizeY = 4, sizeX = 3;
-    int table1[sizeY][sizeX] = {{1, 3, 0}, {1, 1, 0}, {3, 3, 0}, {1, 8, 4}};
-    int table2[sizeY][sizeX] = {{2, 4, 8}, {11, 66, 2}, {38, 2, 43}, {12, -34, 1}};
+    int* addr[global_nlines] = {};
 
-    Matrix_sum((int*)table1, (int*)table2, sizeY, sizeX);
+    int line_lens[global_nlines] = {};
+
+    if (Input(addr, line_lens) == -1) {
+        printf("Error while input\n");
+        return 0;
+    }
+
+    Output(addr, line_lens);
+
+    Free(addr);
+}
+
+int Input(int** addr, int* line_lens) {
+    for (int i = 0; i < global_nlines; ++i) {
+        printf("Enter length of line %d:\n", i+1);
+        scanf("%d", &(line_lens[i]));
+
+        addr[i] = (int*) calloc(line_lens[i], sizeof(int));
+
+        if (addr[i] == NULL) {
+            printf("Error line %d\n", i+1);
+            return -1;
+        }
+
+        printf("Enter line %d:\n", i+1);
+
+        for (int j = 0; j < line_lens[i]; j++) {
+            scanf("%d", addr[i] + j);
+        }
+    }
+    return 0;
+}
+
+void Output(int** addr, int* line_lens)
+{
+    for (int i = 0; i < global_nlines; ++i) {
+        for (int j = 0; j < line_lens[i]; j++) {
+            printf("%3d ", *(addr[i] + j));
+        }
+        printf("\n");
+    }
+}
+
+void Free(int** addr)
+{
+    for (int i = 0; i < global_nlines; ++i) {
+        free(addr[i]);
+        addr[i] = NULL;
+    }
 }
