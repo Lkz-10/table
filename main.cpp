d6fd5eee8a2ebde0b4828a193b9679148e2c6ce9
Lkz-10 <gkazarin.10@bk.ru>
2024-08-28 17:12:55 +0300

    +ввод количества строк

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Input(int** addr, int* line_lens);
-void Output(int** addr, int* line_lens);
+int Input(int** addr, int* line_lens, int nlines);
+void Output(int** addr, int* line_lens, int nlines);
 
-void Free(int** addr);
-
-const int global_nlines = 4;
+void Free(int** addr, int nlines);
 
 int main()
 {
-    int* addr[global_nlines] = {};
+    int nlines = 0;
 
-    int line_lens[global_nlines] = {};
+    printf("Enter amount of lines:\n");
+    scanf("%d", &nlines);
 
-    if (Input(addr, line_lens) == -1) {
+    int** addr = (int**) calloc(nlines, sizeof(int*));
+
+    int* line_lens = (int*) calloc(nlines, sizeof(int));
+
+    if (Input(addr, line_lens, nlines) == -1) {
         printf("Error while input\n");
         return 0;
     }
 
-    Output(addr, line_lens);
+    Output(addr, line_lens, nlines);
 
-    Free(addr);
+    Free(addr, nlines);
+    free(addr);
+    free(line_lens);
 }
 
-int Input(int** addr, int* line_lens) {
-    for (int i = 0; i < global_nlines; ++i) {
+int Input(int** addr, int* line_lens, int nlines) {
+
+    for (int i = 0; i < nlines; ++i) {
+
         printf("Enter length of line %d:\n", i+1);
         scanf("%d", &(line_lens[i]));
 
@@ -45,9 +52,9 @@ int Input(int** addr, int* line_lens) {
     return 0;
 }
 
-void Output(int** addr, int* line_lens)
+void Output(int** addr, int* line_lens, int nlines)
 {
-    for (int i = 0; i < global_nlines; ++i) {
+    for (int i = 0; i < nlines; ++i) {
         for (int j = 0; j < line_lens[i]; j++) {
             printf("%3d ", *(addr[i] + j));
         }
@@ -55,9 +62,9 @@ void Output(int** addr, int* line_lens)
     }
 }
 
-void Free(int** addr)
+void Free(int** addr, int nlines)
 {
-    for (int i = 0; i < global_nlines; ++i) {
+    for (int i = 0; i < nlines; ++i) {
         free(addr[i]);
         addr[i] = NULL;
     }
